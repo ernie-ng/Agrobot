e603fb8c4c4e274f9e2ae889f769336d092d1816
ernie-ng <ernie_ng@outlook.com>
2022-04-01 13:04:07 -0700

    project update april 1

---
+++ Arduino/stepperMotor/stepperMotor.ino
@@ -0,0 +1,38 @@
+
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+//orange wire is signal
+//brown wire is gnd, red is Vcc.
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+
+  for (pos = 90; pos <= 180; pos += 1) { // goes from 90 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(500); //wait  0.5s before moving again.
+  for (pos = 180; pos >= 90; pos -= 1) { // goes from 180 degrees to 90 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(500); //wait  0.5s before moving again.
+}
